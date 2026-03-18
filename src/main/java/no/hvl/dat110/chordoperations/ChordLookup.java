76257fdc3ede3829efddaf345e1e8377657b809c
Leah <799043@stud.hvl.no>
2026-03-18 20:45:04 +0100

    Forslag oppg 1, 2 og 3
    Nesten i mål med oppg 1
    Filemanager replica test teller ikke riktig
    Chordlookup startet på

--- src/main/java/no/hvl/dat110/chordoperations/ChordLookup.java
+++ src/main/java/no/hvl/dat110/chordoperations/ChordLookup.java
@@ -35,16 +35,20 @@ public class ChordLookup {
 		// ask this node to find the successor of key
 		
 		// get the successor of the node
-		
+		NodeInterface successor = node.getSuccessor();
 		// check that key is a member of the set {nodeid+1,...,succID} i.e. (nodeid+1 <= key <= succID) using the checkInterval
 		
 		// if logic returns true, then return the successor
-		
+		if () {
+			
+		}
 		// if logic returns false; call findHighestPredecessor(key)
-		
+		if () {
+			findHighestPredecessor(key);
+		}
 		// do highest_pred.findSuccessor(key) - This is a recursive call until logic returns true
 				
-		return null;					
+		return highest_pred.findSuccessor(key);					
 	}
 	
 	/**
--- src/main/java/no/hvl/dat110/util/Hash.java
+++ src/main/java/no/hvl/dat110/util/Hash.java
@@ -13,6 +13,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class Hash { 
 	
+	private byte[] digest;
 	
 	public static BigInteger hashOf(String entity) {	
 		
@@ -21,15 +22,25 @@ public class Hash {
 		// Task: Hash a given string using MD5 and return the result as a BigInteger.
 		
 		// we use MD5 with 128 bits digest
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 		
 		// compute the hash of the input 'entity'
+//		byte[] digest = md.digest();
+		md.update(entity.getBytes());
+		digest = md.digest();
 		
 		// convert the hash into hex format
+		String hex = toHex(digest);
 		
 		// convert the hex into BigInteger
+		hashint = new BigInteger(hex,16);
 		
 		// return the BigInteger
-		
 		return hashint;
 	}
 	
@@ -38,12 +49,13 @@ public class Hash {
 		// Task: compute the address size of MD5
 		
 		// compute the number of bits = bitSize()
+		int bitsize = 0;
 		
 		// compute the address size = 2 ^ number of bits
+		BigInteger addressSize = BigInteger.valueOf((long) Math.pow(2, bitsize));
 		
 		// return the address size
-		
-		return null;
+		return addressSize;
 	}
 	
 	public static int bitSize() {
@@ -51,6 +63,7 @@ public class Hash {
 		int digestlen = 0;
 		
 		// find the digest length
+		digestlen = digest.length();
 		
 		return digestlen*8;
 	}
